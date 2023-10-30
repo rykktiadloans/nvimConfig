08ae45a39aa97730cf2ac960310bd809cda3795c
rykktiadLoans <datokar1705@gmail.com>
2023-10-30 23:54:46 +0100

    added lualine plus some other stuff

---
+++ lua/rl/plugins/lualine.lua
@@ -0,0 +1,54 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local colors = require("dracula").colors()
+		local draculatheme = {
+			normal = {
+				a = { bg = colors.cyan, fg = colors.bg },
+				b = { bg = colors.menu, fg = colors.fg },
+				c = { bg = colors.bg , fg = colors.fg },
+			},
+			insert = {
+				a = { bg = colors.green, fg = colors.bg },
+				b = { bg = colors.menu, fg = colors.fg },
+				c = { bg = colors.bg , fg = colors.fg },
+			},
+			visual = {
+				a = { bg = colors.pink, fg = colors.bg },
+				b = { bg = colors.menu, fg = colors.fg },
+				c = { bg = colors.bg , fg = colors.fg },
+			},
+			replace = {
+				a = { bg = colors.orange, fg = colors.fg },
+				b = { bg = colors.menu, fg = colors.fg },
+				c = { bg = colors.bg , fg = colors.fg },
+			},
+			command = {
+				a = { bg = colors.red, fg = colors.fg },
+				b = { bg = colors.menu, fg = colors.fg },
+				c = { bg = colors.bg , fg = colors.fg },
+			},
+			inactive = {
+				a = { bg = colors.yellow, fg = colors.fg },
+				b = { bg = colors.menu, fg = colors.fg },
+				c = { bg = colors.bg , fg = colors.fg },
+			},
+		}
+		require("lualine").setup({
+			options = {
+				theme = draculatheme,
+                section_separators = {},
+                component_separators = {left = "/", right = "/"},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics", "searchcount" },
+				lualine_c = { "vim.api.nvim_buf_get_name(0)" },
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location", "os.date('%H:%M:%S %d %b')" },
+			},
+		})
+	end,
+}
--- lua/rl/remap.lua
+++ lua/rl/remap.lua
@@ -1,13 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<C-p>", "\"+p")
-vim.keymap.set({"n", "v"}, "<C-y>", "\"+y")
-vim.keymap.set({"n", "v"}, "<C-Y>", "\"+Y")
+vim.keymap.set({"n", "v"}, "<leader>p", "\"+p")
+vim.keymap.set({"n", "v"}, "<leader>y", "\"+y")
+vim.keymap.set({"n", "v"}, "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>nh", vim.cmd.noh)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', 'Q', "<nop>")
 
-vim.keymap.set('n', '<leader>b', "``")
-
+vim.keymap.set('n', '<leader>b', "<nop>")
+vim.keymap.set({'n', "v"}, "<leader>w", "<C-w>")
