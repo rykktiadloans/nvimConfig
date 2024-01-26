be3ac83d4db4a3f58edc9fa9e252ba10f9ac57cc
rykktiadLoans <datokar1705@gmail.com>
2024-01-26 20:02:35 +0100

    added cmake support

---
+++ lua/rl/plugins/haskell.lua
@@ -0,0 +1,5 @@
+return {
+    "MrcJkb/haskell-tools.nvim",
+    version = '^3', -- Recommended
+    ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
+}
--- lua/rl/plugins/lspconfig.lua
+++ lua/rl/plugins/lspconfig.lua
@@ -7,7 +7,7 @@ return{
 
 		-- Global mappings.
 		-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-		vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+		vim.keymap.set('n', '<leader>fd', vim.diagnostic.open_float)
 		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 		vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 		vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
